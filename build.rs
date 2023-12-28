4b31ac75b02a361e7370393d07dcfa43dbaea79f
慕梓茜 <voidwind@qq.com>
2023-12-28 23:11:03 +0800

    试作：黑金风格
    Signed-off-by: 慕梓茜 <voidwind@qq.com>

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
 fn main() {
     slint_build::compile("ui/app.slint").unwrap();
+    embed_resource::compile("icon.rc", embed_resource::NONE);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![windows_subsystem = "windows"]
 
+use slint::LogicalPosition;
+
 use label::LabelPath;
 
 mod label;
@@ -14,8 +16,23 @@ fn main() -> Result<(), slint::PlatformError> {
         let ui = ui_handle.unwrap();
         ui.set_counter(ui.get_counter() + 1);
     });
-    let ui_handle = ui.as_weak();
 
+    let ui_handle = ui.as_weak();
+    ui.on_close_window(move || {
+        let ui = ui_handle.unwrap();
+        ui.hide().unwrap();
+    });
+
+    let ui_handle = ui.as_weak();
+    ui.on_move_window(move |offset_x, offset_y| {
+        let ui = ui_handle.unwrap();
+        //获取窗口的物理坐标
+        let logical_pos = ui.window().position().to_logical(ui.window().scale_factor());
+        //窗口坐标添加上偏移量，设置为新的位置
+        ui.window().set_position(LogicalPosition::new(logical_pos.x + offset_x, logical_pos.y + offset_y));
+    });
+
+    let ui_handle = ui.as_weak();
     ui.on_label_res(move || {
         let ui = ui_handle.unwrap();
         let label_value = label::new(LabelPath {
@@ -25,6 +42,7 @@ fn main() -> Result<(), slint::PlatformError> {
         });
         ui.set_label_check_value(label_value.0.into());
         ui.set_label_check_color(label_value.1.into());
+        ui.set_window_hight(255.0)
     });
 
     ui.run()
